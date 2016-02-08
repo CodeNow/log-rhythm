580079f585275bdaae8c2f51b4496d0b9f35ff73
Anandkumar Patel <anand@runnable.com>
2016-02-08 14:19:41 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# log-rhythm
+used to test CMD logs
+
+command outputs file based on ENV variable configuration
+
+`LINE_SIZE_IN_BYTES`: size of each line(should be multiple)
+`NUM_LINES`: number of lines
+
+entry point `./run.sh <LINE_SIZE_IN_BYTES> <NUM_LINES>`
+
+# Requirements
+
+* base64
+* head
+* /dev/urandom
---
+++ run.sh
@@ -0,0 +1,17 @@
+if [[ $1 = '' ]]; then
+  echo 'missing LINE_SIZE_IN_BYTES (run.sh <LINE_SIZE_IN_BYTES> <NUM_LINES>)'
+  exit 1
+fi
+
+LINE_SIZE_IN_BYTES="$1"
+
+if [[ $2 = '' ]]; then
+  echo 'missing NUM_LINES (run.sh <LINE_SIZE_IN_BYTES> <NUM_LINES>)'
+  exit 1
+fi
+
+NUM_LINES="$2"
+
+for LINE in `seq 1 $NUM_LINES`; do
+  base64 /dev/urandom | head -c $LINE_SIZE_IN_BYTES;
+done
